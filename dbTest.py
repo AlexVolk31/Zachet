df4387f6ce57523411de7f4d52ef63b65d986bfb
LexaVolk <aleks.volk.04@mail.ru>
2022-01-19 23:33:16 +0300

    edit

--- dbTest.py
+++ dbTest.py
@@ -2,13 +2,13 @@ import unittest
 from pyTOsql.py import *
 
 class TestUM(unittest.TestCase):
-    def setUp(self):
+    def setUpModule(self):
         self.con = sqlite3.connect('Data.db')
         self.cursor = self.con.cursor()
         init_db(self.cursor)
         self.con.commit()
 
-    def tearDown(self):
+    def tearDownModule(self):
         self.con.commit()
         self.con.close()
 
